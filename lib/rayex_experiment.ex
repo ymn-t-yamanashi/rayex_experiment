1a37c5ffecca17546ef9c1ce7fc319bf0559ad84
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-18 17:45:36 +0900

    サンプル

--- lib/rayex_experiment.ex
+++ lib/rayex_experiment.ex
@@ -2,18 +2,29 @@ defmodule RayexExperiment do
   use Rayex
 
   def main() do
-    init_window(640, 480, "window name")
+    init_window(800, 800, "window name")
     main_loop(true)
   end
 
   defp main_loop(true) do
     begin_drawing()
-    draw_line(10, 10, 50, 51, %{r: 255, g: 161, b: 0, a: 255})
+    draw()
+    #draw_line(10, 10, 50, 51, %{r: 255, g: 161, b: 0, a: 255})
     end_drawing()
-    Process.sleep(500)
+
+    Process.sleep(10)
     main_loop(!window_should_close())
   end
 
   defp main_loop(_), do: nil
+
+  defp draw() do
+    x1 = Enum.random(1..800)
+    y1 = Enum.random(1..800)
+    x2 = Enum.random(1..800)
+    y2 = Enum.random(1..800)
+
+    draw_line(x1, y1, x2, y2, %{r: 255, g: 161, b: 0, a: 255})
+  end
 
 end
