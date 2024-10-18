34217d76a4c4a0ea8c3aa436e6adef5809a8e046
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-18 17:31:43 +0900

    サンプル作成

--- lib/rayex_experiment.ex
+++ lib/rayex_experiment.ex
@@ -1,18 +1,19 @@
 defmodule RayexExperiment do
-  @moduledoc """
-  Documentation for `RayexExperiment`.
-  """
+  use Rayex
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> RayexExperiment.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main() do
+    init_window(640, 480, "window name")
+    main_loop(true)
   end
+
+  defp main_loop(true) do
+    begin_drawing()
+    draw_line(10, 10, 50, 51, %{r: 255, g: 161, b: 0, a: 255})
+    end_drawing()
+    Process.sleep(500)
+    main_loop(!window_should_close())
+  end
+
+  defp main_loop(_), do: nil
+
 end
