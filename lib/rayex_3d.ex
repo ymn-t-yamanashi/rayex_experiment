a673e0c01a89c72f9d88ebb7c6a630112795eaf5
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-10-19 11:00:37 +0900

    rayex_3d修正

--- lib/rayex_3d.ex
+++ lib/rayex_3d.ex
@@ -4,11 +4,10 @@ defmodule Rayex3d do
 
   @color_white %S.Color{r: 245, g: 245, b: 245, a: 255}
   @color_gray %S.Color{r: 130, g: 130, b: 130, a: 255}
-  @color_maroon %S.Color{r: 190, g: 33, b: 55, a: 255}
 
   def main() do
     init_window(800, 800, "window name")
-    set_target_fps(60)
+    set_target_fps(240)
     main_loop(true, -10.0)
   end
 
@@ -17,10 +16,7 @@ defmodule Rayex3d do
 
     begin_drawing()
     draw(c)
-    # draw_line(10, 10, 50, 51, %{r: 255, g: 161, b: 0, a: 255})
     end_drawing()
-
-    Process.sleep(10)
     main_loop(!window_should_close(), c)
   end
 
@@ -34,21 +30,14 @@ defmodule Rayex3d do
       target: %S.Vector3{x: 0.0, y: 0.0, z: 0.0},
       up: %S.Vector3{x: 0.0, y: 1.0, z: 0.0},
       fovy: 45.0,
-      # perspective projection
       projection: 0
     }
 
     update_camera(camera, 4)
-
     cube_position = %S.Vector3{x: c, y: c, z: 1.0}
     begin_mode_3d(camera)
-    # draw_cube_wires(cube_position, 2.0, 2.0, 2.0, @color_gray)
-    draw_cube(cube_position, 1.0, 1.0, 1.0, @color_gray)
-
-    ray = get_mouse_position() |> get_mouse_ray(camera)
-    draw_ray(ray, @color_maroon)
-
-    draw_grid(10, 1.0)
+    draw_cube(cube_position, 0.1, 0.1, 0.1, @color_gray)
+    draw_grid(100, 1.0)
     end_mode_3d()
   end
 end
